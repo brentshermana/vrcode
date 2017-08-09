1903531a6e981a081d555b38afee162a58458f78
Brent Allard <brentshermana@Brents-MacBook-Pro.local>
2017-08-09 16:49:23 -0400

    rest of files

---
+++ Assets/Scripts/NetworkingClientDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Net.Sockets;
+using System.Net;
+
+public class NetworkingClientDemo : MonoBehaviour {
+
+	private IPHostEntry localhost;
+	private TcpClient client;
+
+	// Use this for initialization
+	void Start () {
+		//localhost = Dns.GetHostEntry (Dns.GetHostName());
+		//IPAddress[] addressList = localhost.AddressList;
+		TcpClient _client = new TcpClient ();
+		_client.BeginConnect (IPAddress.Loopback, 5555, new AsyncCallback(acceptCallback), _client);
+	}
+
+	private void acceptCallback(IAsyncResult result) {
+		if (result.IsCompleted) {
+			Debug.Log("Client Completed!");
+			TcpClient _client = (TcpClient)result.AsyncState;
+			_client.EndConnect (result);
+			client = _client; //this is where client is initialized
+			Debug.Log("Client connected");
+		}
+		else {
+			Debug.Log("Error!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/NetworkingServerDemo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class NetworkingServerDemo : MonoBehaviour {
+
+
+	private string buf2str (byte[] buf) {
+		return System.Text.Encoding.UTF8.GetString (buf);
+	}
+	private byte[] str2buf (string str) {
+		return System.Text.Encoding.UTF8.GetBytes (str);
+	}
+
+	private IPHostEntry localhost;
+
+	// Use this for initialization
+	void Start () {
+		//localhost = Dns.GetHostEntry (Dns.GetHostName());
+		//IPAddress[] addressList = localhost.AddressList;
+		//Debug.Log ("Length " + addressList.Length);
+		TcpListener listener = new TcpListener(IPAddress.Loopback, 5555);
+		listener.Start ();
+
+		IAsyncResult task = listener.BeginAcceptTcpClient (new AsyncCallback(acceptCallback), listener);
+	}
+
+	private void acceptCallback(IAsyncResult result) {
+		if (result.IsCompleted) {
+			Debug.Log("ServerCompleted!");
+			TcpListener listener = (TcpListener)result.AsyncState;
+			listener.EndAcceptTcpClient (result);
+			Debug.Log ("Server Connect");
+		}
+		else {
+			Debug.Log("Error!");
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
