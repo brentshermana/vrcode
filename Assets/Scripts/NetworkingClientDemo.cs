393703b9daf0482a42556a87d727db909401744b
brentshermana <brentshermana@gmail.com>
2017-08-20 15:40:50 -0400

    adapt networking example to use json. next step is to extract into class

--- Assets/Scripts/NetworkingClientDemo.cs
+++ Assets/Scripts/NetworkingClientDemo.cs
@@ -6,15 +6,40 @@ using System;
 using System.Net.Sockets;
 using System.Net;
 
+using Newtonsoft.Json;
+
 public class NetworkingClientDemo : MonoBehaviour {
 
 	private IPHostEntry localhost;
 	private TcpClient client;
+    private Socket sock;
+    private byte[] buffer;
 
-	// Use this for initialization
-	void Start () {
-		//localhost = Dns.GetHostEntry (Dns.GetHostName());
-		//IPAddress[] addressList = localhost.AddressList;
+    private string buf2str(byte[] buf)
+    {
+        return System.Text.Encoding.UTF8.GetString(buf);
+    }
+    private byte[] str2buf(string str)
+    {
+        return System.Text.Encoding.UTF8.GetBytes(str);
+    }
+
+    // Use this for initialization
+    void Start () {
+        //localhost = Dns.GetHostEntry (Dns.GetHostName());
+        //IPAddress[] addressList = localhost.AddressList;
+
+        //prep the json message to send
+        ExampleJsonClass obj = new ExampleJsonClass(4, "Hello World!");
+        string jsonstr = JsonConvert.SerializeObject(obj);
+        byte[] json_buf = str2buf(jsonstr);
+        byte[] sizebytes = BitConverter.GetBytes(json_buf.Length);
+        int size_len = sizebytes.Length; //presumably 4
+        if (BitConverter.IsLittleEndian) Array.Reverse(sizebytes); //big endian
+        Array.Resize<byte>(ref sizebytes, size_len + json_buf.Length);
+        Array.Copy(json_buf, 0, sizebytes, size_len, json_buf.Length);
+        buffer = sizebytes;
+
 		TcpClient _client = new TcpClient ();
 		_client.BeginConnect (IPAddress.Loopback, 5555, new AsyncCallback(acceptCallback), _client);
 	}
@@ -25,12 +50,26 @@ public class NetworkingClientDemo : MonoBehaviour {
 			TcpClient _client = (TcpClient)result.AsyncState;
 			_client.EndConnect (result);
 			client = _client; //this is where client is initialized
-			Debug.Log("Client connected");
+
+            sock = client.Client;
+            
+            Debug.Log("Client connected");
+
+            Debug.Log("Client Begin Send");
+
+            sock.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(sendcomplete), new object());
+			
 		}
 		else {
 			Debug.Log("Error!");
 		}
 	}
+
+    private void sendcomplete(IAsyncResult result)
+    {
+        Debug.Log("Client Finished Sending");
+        sock.EndSend(result);
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/NetworkingServerDemo.cs
+++ Assets/Scripts/NetworkingServerDemo.cs
@@ -7,7 +7,14 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
+
+using Newtonsoft.Json;
+
 public class NetworkingServerDemo : MonoBehaviour {
+
+    private static int MIN_BUF = 2000;
+
+    private Socket sock;
 
 
 	private string buf2str (byte[] buf) {
@@ -17,13 +24,18 @@ public class NetworkingServerDemo : MonoBehaviour {
 		return System.Text.Encoding.UTF8.GetBytes (str);
 	}
 
+    private byte[] buffer;
+
 	private IPHostEntry localhost;
 
 	// Use this for initialization
 	void Start () {
-		//localhost = Dns.GetHostEntry (Dns.GetHostName());
-		//IPAddress[] addressList = localhost.AddressList;
-		//Debug.Log ("Length " + addressList.Length);
+        //localhost = Dns.GetHostEntry (Dns.GetHostName());
+        //IPAddress[] addressList = localhost.AddressList;
+        //Debug.Log ("Length " + addressList.Length);
+
+        buffer = new byte[4];
+
 		TcpListener listener = new TcpListener(IPAddress.Loopback, 5555);
 		listener.Start ();
 
@@ -32,15 +44,44 @@ public class NetworkingServerDemo : MonoBehaviour {
 
 	private void acceptCallback(IAsyncResult result) {
 		if (result.IsCompleted) {
-			Debug.Log("ServerCompleted!");
+			Debug.Log("Server Completed!");
 			TcpListener listener = (TcpListener)result.AsyncState;
-			listener.EndAcceptTcpClient (result);
+			TcpClient client = listener.EndAcceptTcpClient (result);
+
+            sock = client.Client;
+            
 			Debug.Log ("Server Connect");
+            
+            sock.BeginReceive(buffer, 0, 4, SocketFlags.None, new AsyncCallback(jsonsizecallback), new object());
 		}
 		else {
 			Debug.Log("Error!");
 		}
 	}
+
+    private void jsonsizecallback(IAsyncResult result)
+    {
+        sock.EndReceive(result);
+
+        //at this point the size of the buffer is 4
+        if (BitConverter.IsLittleEndian) Array.Reverse(buffer);
+        int size = BitConverter.ToInt32(buffer, 0);
+        Debug.Log("Server: Size is " + size);
+        buffer = new byte[size];
+
+        sock.BeginReceive(buffer, 0, size, SocketFlags.None, new AsyncCallback(jsoncallback), size);
+    }
+
+    private void jsoncallback(IAsyncResult result)
+    {
+        int size = (int)result.AsyncState;
+        Debug.Log("Server: parsing json of size " + size);
+        sock.EndReceive(result);
+        string jsonstr = buf2str(buffer);
+        ExampleJsonClass obj = JsonConvert.DeserializeObject<ExampleJsonClass>(jsonstr);
+
+        Debug.Log("Server: Json: " + obj.ToString());
+    }
 
 	
 	// Update is called once per frame
