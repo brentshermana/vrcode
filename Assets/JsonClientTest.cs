1df2c512925c82c163734e225220c318c55415f6
brentshermana <brentshermana@gmail.com>
2017-08-22 18:05:17 -0400

    removed errors, but code is not working as expected

---
+++ Assets/JsonClientTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonClientTest : MonoBehaviour {
+
+    private JsonClient client;
+    private QueueWithSem<ActionableJsonMessage> q;
+    private ActionableJsonMessage msg;
+    private bool success = false;
+
+	// Use this for initialization
+	void Start () {
+        msg = new ActionableJsonMessage();
+        q = new QueueWithSem<ActionableJsonMessage>();
+        client = new JsonClient(process, q);
+        success = q.putNoWait(msg);
+	}
+
+    void process(ActionableJsonMessage message)
+    {
+        Debug.Log("Client Processing Json Message: " + message);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (!success)
+        {
+            success = q.putNoWait(msg);
+        }
+	}
+}
---
+++ Assets/JsonServerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonServerTest : MonoBehaviour {
+
+    private JsonServer server;
+
+    private bool success = false;
+
+    private QueueWithSem<ActionableJsonMessage> q;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Server Test Init");
+        q = new QueueWithSem<ActionableJsonMessage>();
+        server = new JsonServer(nothing, q);
+        success = q.putNoWait(new ActionableJsonMessage());
+	}
+
+    void nothing(ActionableJsonMessage message)
+    {
+        Debug.Log("Success Read!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (! success)
+        {
+            success = q.putNoWait(new ActionableJsonMessage());
+        }
+	}
+}
