c3deebdf45a983cde322cf457a751d1ab71c0f9b
brentshermana <brentshermana@gmail.com>
2017-08-23 13:35:33 -0400

    figured out netmq hanging issues, still need to remove some obsolete files

---
+++ Assets/Scripts/monobehaviours/networking/RealNetMqServer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NetMQ;
+using NetMQ.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+
+public class RealNetMqServer : MonoBehaviour {
+
+    private ConcurrentQueue<ActionableJsonMessage> inqueue = new ConcurrentQueue<ActionableJsonMessage>();
+    private ConcurrentQueue<ActionableJsonMessage> outqueue = new ConcurrentQueue<ActionableJsonMessage>();
+
+    private NetMQThread actor;
+
+	// Use this for initialization
+	void Start () {
+        actor = new NetMQThread(inqueue, outqueue);
+        actor.Start();
+	}
+
+    private void OnDestroy()
+    {
+        actor.Stop();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //TODO: act on messages!
+        bool success = true;
+        while (success)
+        {
+            ActionableJsonMessage msg = inqueue.TryDequeue(ref success);
+            if (success)
+            {
+                UnityEngine.Debug.Log("Server Received a Message -> " + msg.ToString());
+            }
+        }
+	}
+}
