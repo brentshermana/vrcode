9eaafd54dcc8c614a3b04f80ca8414e3c4efa02d
Brent Allard <brentshermana@Brents-MacBook-Pro.local>
2017-09-05 09:14:37 -0400

    3d string capabilities

---
+++ Assets/LetterScope.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterScope : MonoBehaviour {
+
+	//extrema
+	private float miny = float.MaxValue;
+	private float maxy = float.MinValue;
+
+	private float minx = float.MaxValue;
+	private float maxx = float.MinValue;
+
+	private float minz = float.MaxValue;
+	private float maxz = float.MinValue;
+
+	private float width;
+	public float Width {
+		get { return width; }
+	}
+
+	// Use this for initialization
+	void Awake () {
+		Quaternion rotation = transform.rotation;
+		transform.rotation = Quaternion.identity;
+
+		SetExtents ();
+		//We assume the characters have no rotation
+		width = Mathf.Abs (maxx-minx);
+		//MakeExtentCube ();
+
+		transform.rotation = rotation;
+
+		Debug.Log ("Letter says its width is " + width);
+	}
+
+	// for demonstrative purposes
+	private void MakeExtentCube() {
+		float xd = Mathf.Abs(minx-maxx);
+		float yd = Mathf.Abs(miny-maxy);
+		float zd = Mathf.Abs(minz-maxz);
+		Vector3 scale = new Vector3 (xd, yd, zd);
+
+		float xc = (maxx + minx) / 2f;
+		float yc = (maxy + miny) / 2f;
+		float zc = (maxz + minz) / 2f;
+		Vector3 center = new Vector3(xc, yc, zc);
+
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = center;
+		cube.transform.localScale = scale; //local scale is fine - no parent
+		cube.transform.rotation = transform.rotation;
+	}
+
+	private void SetExtents() {
+		List<Vector3> vertices = new List<Vector3> ();
+		Queue<Transform> q = new Queue<Transform> ();
+		q.Enqueue (transform);
+		while (q.Count > 0) {
+			Transform current = q.Dequeue ();
+			// if the current transform has a mesh, get it
+			MeshFilter mf = current.gameObject.GetComponent<MeshFilter> ();
+			if (mf != null) {
+				Mesh m = mf.mesh;
+				foreach (Vector3 vl in m.vertices) {
+					Vector3 v = current.TransformPoint (vl); //conv to global space
+
+					float x = v.x;
+					maxx = Mathf.Max (maxx, x);
+					minx = Mathf.Min (minx, x);
+
+					float y = v.y;
+					maxy = Mathf.Max (maxy, y);
+					miny = Mathf.Min (miny, y);
+
+					float z = v.z;
+					maxz = Mathf.Max (maxz, z);
+					minz = Mathf.Min (minz, z);
+				}
+			}
+			// add all children to queue
+			foreach (Transform child in current) {
+				q.Enqueue (child);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/String3D.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class String3D : MonoBehaviour {
+
+	private static Dictionary<char, GameObject> characters;
+	private static HashSet<char> unavailable_chars;
+
+
+
+	private List<char> chars;
+	private List<LetterScope> letters;
+	private float x_offset;
+
+	public bool capture_keyboard;
+	public string resource_subdir; //System.IO.Path.PathSeparator + "Characters" + System.IO.Path.PathSeparator + "_";
+
+	// Use this for initialization
+	void Awake () {
+		x_offset = 0;
+		characters = new Dictionary<char, GameObject> ();
+		unavailable_chars = new HashSet<char> ();
+		chars = new List<char> ();
+		letters = new List<LetterScope> ();
+
+		Debug.Log (resource_subdir);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (capture_keyboard) {
+			foreach (char c in Input.inputString) {
+				//check for backspace:
+				if (c == '\b') {
+					Backspace ();
+					continue;
+				}
+				else if (unavailable_chars.Contains (c)) {
+					Debug.Log (c.ToString() + " Is known to be invalid");
+					continue;
+				}
+				else {
+					//attempt to load prefab in 'resource_subdir' by this name
+					string path = resource_subdir + c.ToString();
+					if (char.IsLetter (c) && char.IsLower (c)) { //because unity's naming within editor isn't case sensitive
+						path += "l";
+					}
+					GameObject charobj = Resources.Load(path) as GameObject;
+
+					if (charobj != null) {
+						Debug.Log ("Loaded Resource : " + c.ToString());
+
+						GameObject instance = Instantiate (charobj) as GameObject;
+						instance.transform.rotation = transform.rotation;
+
+						chars.Add (c);
+						LetterScope letter = instance.GetComponent<LetterScope> ();
+
+						Debug.Log ("String3D reads width " + letter.Width);
+						x_offset += .5f * letter.Width;
+						Vector3 center = transform.position + -transform.right * x_offset; //left because Unity's coordinate system is odd
+						instance.transform.position = center;
+						x_offset += .5f * letter.Width;
+
+						Debug.Log ("Offset " + x_offset + " right: " + transform.right);
+
+						letters.Add (letter);
+					} else {
+						Debug.Log ("Failed to Load Resource : " + c.ToString());
+						unavailable_chars.Add (c);
+					}
+				}
+			}
+		}
+	}
+
+	void Backspace() {
+		int index = chars.Count - 1;
+		if (index < 0)
+			return;
+		x_offset -= letters [index].Width;
+		Destroy (letters [index].gameObject);
+		chars.RemoveAt (index);
+		letters.RemoveAt (index);
+	}
+}
