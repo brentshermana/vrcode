6f5086167cc8c9ef8b5bc2ea0333da8ec8db68c2
brentshermana <brentshermana@gmail.com>
2018-01-14 15:15:06 -0800

    Added Leap Motion SDK. It doesn't have things I'd hoped for like gesture recognition, but does have a bunch of cool prebuilt buttons and sliders, etc. that I can use

---
+++ Assets/LeapMotionGestures.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Leap;
+
+public class LeapMotionGestures : MonoBehaviour {
+
+    private Controller controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = new Controller();
+        controller.FrameReady += MyOnFrame;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    static void MyOnFrame(object sender, FrameEventArgs args)
+    {
+        Frame frame = args.frame;
+        foreach (Hand hand in frame.Hands)
+            HandleHand(hand);
+    }
+    static void HandleHand(Hand hand)
+    {
+        
+    }
+}
