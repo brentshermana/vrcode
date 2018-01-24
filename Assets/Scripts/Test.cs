176584e37dbf9ef0ecb696c881a7fedfce9858e9
brentshermana <brentshermana@gmail.com>
2018-01-24 11:06:11 -0800

    new scene for gui testing

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour {
 
     public int i;
 
+    public InputField inputField;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
