9c20d92a97390525f0752f41021e861e5ba9ba7c
brentshermana <brentshermana@gmail.com>
2018-01-26 13:55:22 -0800

    partway through playing around with eventsystem, input module

---
+++ Assets/Scripts/StandaloneInputModuleCustom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+public class StandaloneInputModuleCustom : CurvedUIInputModule {
+ 
+    public PointerEventData GetLastPointerEventDataPublic (int id)
+    {
+        return GetLastPointerEventData(id);
+    }
+}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,39 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using TMPro;
 
 public class Test : MonoBehaviour {
 
-    public int i;
+    private int i;
+    private int j;
 
     public InputField inputField;
+    public TMP_InputField tmField;
+    public EventSystem es;
 
-	// Use this for initialization
-	void Start () {
+    public StandaloneInputModuleCustom inputModule;
+
+    public GraphicRaycaster raycaster;
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log((int)'a');
-        Debug.Log((int)'b');
-        //var buttons = SteamVR_Controller.Input(i);
+        //tmField.textComponent.textInfo.characterInfo[0].y
+        if (tmField.isFocused)
+        {
+            i = tmField.selectionFocusPosition;
+            j = tmField.selectionStringFocusPosition;
+            Debug.Log("Selection Focus: " + i + " String Focus: " + j);
+        }
 
-        //if ( buttons.GetPress(SteamVR_Controller.ButtonMask.Trigger) )//buttons.GetHairTrigger();
+        Vector3 mouseOrigin = Input.mousePosition;
+        mouseOrigin.z = 0.0f;
+        mouseOrigin = Camera.main.ScreenToWorldPoint(mouseOrigin);
+
+        int layer = 1 << LayerMask.NameToLayer("UI");
+
+        //Ray ray = new Ray(mouseOrigin, Camera.main.transform.forward);
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit))
         //{
-        //    Debug.Log("Trigger");
+        //    Debug.Log("Hit at " + hit.point);
         //}
 
+        Vector3 mp = Input.mousePosition;
+
+        
+
+        
+
     }
-    public void OnGUI()
+
+    void OnGUI()
     {
-        //Debug.Log("OnGUI!");
-        //var ev = Event.current;
-        //if (ev.type != EventType.KeyDown && ev.type != EventType.KeyUp) return;
-
-        ////		if (ev.character != 0) Debug.Log("ev >>> " + ev.character);
-        ////		else if (ev.type == EventType.KeyUp) Debug.Log("ev ^^^ " + ev.keyCode);
-        ////		else if (ev.type == EventType.KeyDown) Debug.Log("ev vvv " + ev.keyCode);
-
-        //keyEvents.Add(new Event(ev));
+        List < RaycastResult > resultList = new List<RaycastResult>();
+        PointerEventData pd = inputModule.GetLastPointerEventDataPublic(-1);
+        if (pd != null) {
+            raycaster.Raycast(pd, resultList);
+            foreach (RaycastResult r in resultList)
+                Debug.Log(r.worldPosition);
+        }
+        
+        //Debug.Log("WP: " + pd.pointerCurrentRaycast.worldPosition);
     }
+
+    //void OnGUI()
+    //{
+    //    Event e = Event.current;
+    //    Debug.Log(e.mousePosition);
+    //}
+
+    //public void OnGUI()
+    //{
+    //    //Debug.Log("OnGUI!");
+    //    //var ev = Event.current;
+    //    //if (ev.type != EventType.KeyDown && ev.type != EventType.KeyUp) return;
+
+    //    ////		if (ev.character != 0) Debug.Log("ev >>> " + ev.character);
+    //    ////		else if (ev.type == EventType.KeyUp) Debug.Log("ev ^^^ " + ev.keyCode);
+    //    ////		else if (ev.type == EventType.KeyDown) Debug.Log("ev vvv " + ev.keyCode);
+
+    //    //keyEvents.Add(new Event(ev));
+    //}
 }
