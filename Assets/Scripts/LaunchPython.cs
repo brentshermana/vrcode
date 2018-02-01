69c4be62e1196f1050050d55f92506725c321eb4
brentshermana <brentshermana@gmail.com>
2018-02-01 12:38:46 -0800

    run python from C#

---
+++ Assets/Scripts/LaunchPython.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Text;
+
+using System.Diagnostics;
+
+public class LaunchPython : MonoBehaviour {
+    public string PythonPath;
+    public string FileToRun;
+
+    private Process process;
+
+	// Use this for initialization
+	void Start () {
+        process = new Process();
+        process.StartInfo.FileName = PythonPath;
+        process.StartInfo.UseShellExecute = false; //necessary for getting stdout,err
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.Arguments = FileToRun;
+        process.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (process.HasExited)
+        {
+            Stdio(process);
+            this.enabled = false;
+        }
+	}
+    void Stdio(Process p)
+    {
+        PrintStream(p.StandardOutput, "Python Stdout");
+        PrintStream(p.StandardError, "Python Stderr");
+    }
+    void PrintStream(System.IO.StreamReader reader, string streamName)
+    {
+        StringBuilder sb = new StringBuilder("Stream " + streamName + ":\n");
+        while (!reader.EndOfStream)
+        {
+            sb.Append((char)reader.Read());
+        }
+        UnityEngine.Debug.Log(sb.ToString());
+    }
+}
